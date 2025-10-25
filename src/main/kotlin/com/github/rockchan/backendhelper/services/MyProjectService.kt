a11f08ab343e866484b93dcba899e37d82c31b05
GitHub Action <action@github.com>
2025-10-25 08:26:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/rockchan/backendhelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rockchan/backendhelper/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.rockchan.backendhelper.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rockchan.backendhelper.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/rockchan/backendhelper/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/rockchan/backendhelper/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.rockchan.backendhelper.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
